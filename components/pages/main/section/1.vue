ee684346bea2adfafeaecd7b8e440c0acc20dcb8
fred <ugodaycnzass1995@gmail.com>
2023-01-08 21:04:13 +0300

    fix

--- components/pages/main/section/1.vue
+++ components/pages/main/section/1.vue
@@ -1,5 +1,5 @@
 <template>
-   <section class="bg-1 h-screen mnBg">
+   <section class="bg-1 sm:h-screen h-600px mnBg">
       <div class="container h-full flex overflow-hidden items-center justify-center">
          <div class="">
             <UiSubtitle>Грозный, Февраль 5-9 2023</UiSubtitle>
--- components/pages/main/section/3.vue
+++ components/pages/main/section/3.vue
@@ -22,4 +22,20 @@ const date = ref([
    ['00', 'Минуты'],
    ['00', 'Секунды'],
 ])
+
+const finishDate = "2020-03-12T00:56:20+03:00"
+
+const timer = setInterval(() => {
+   const dif = new Date((Date.parse(finishDate) - new Date()))
+   if (dif != 'Invalid Date') {
+      date.value[0][0] = dif.getMonth() < 10 ? '0' + dif.getMonth() : dif.getMonth()
+      date.value[1][0] = dif.getDate() < 10 ? '0' + dif.getDate() : dif.getDate()
+      date.value[2][0] = dif.getHours() < 10 ? '0' + dif.getHours() : dif.getHours()
+      date.value[3][0] = dif.getMinutes() < 10 ? '0' + dif.getMinutes() : dif.getMinutes()
+      date.value[4][0] = dif.getSeconds() < 10 ? '0' + dif.getSeconds() : dif.getSeconds()
+   } else {
+      clearInterval(timer)
+   }
+}, 1000);
+
 </script>
