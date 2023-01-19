c6c53a856415a122f67997b6272f38ba4bab7102
fred <ugodaycnzass1995@gmail.com>
2023-01-19 18:30:17 +0300

    fix

--- app.vue
+++ app.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="bg-base-2">
-    <div class="bg-0 text-base-1 bg-fixed">
-      <NuxtLayout>
-        <NuxtPage />
-      </NuxtLayout>
-    </div>
+  <div class="bg-0 text-base-1 bg-fixed">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
 
