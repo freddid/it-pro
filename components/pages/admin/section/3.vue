3fb3807f09cbf4122d26c04ba917f763af00d872
fred <ugodaycnzass1995@gmail.com>
2023-01-20 10:08:13 +0300

    fix

--- components/pages/admin/section/3.vue
+++ components/pages/admin/section/3.vue
@@ -1,27 +1,29 @@
 <template>
-   <div class="space-y-20px">
-      <div class="flex justify-end">
-         <button @click="actNews = 'N', title = '', content = ''"
-            class="p-15px bg-base-3 rounded-[5px] text-base-2">Добавить
-            новость</button>
-      </div>
-      <div v-for="item in state.getNewsObj" :key="item._id"
-         class="flex w-full justify-between rounded-[5px] p-5px bg-base-1 text-base-2">
-         <div class="flex items-center pl-20px text-20px">
-            {{ item.title }}
+   <div>
+      <div class="space-y-20px">
+         <div class="flex justify-end">
+            <button @click="actNews = 'N', title = '', content = ''"
+               class="p-15px bg-base-3 rounded-[5px] text-base-2">Добавить
+               новость</button>
          </div>
-         <div class="flex space-x-10px children:(cursor-pointer p-10px bg-base-3 rounded-[3px])">
-            <button @click="actNews = item">
-               <img src="@/assets/imgs/icons/edit.png" alt="">
-            </button>
-            <button class="disabled:opacity-40" :disabled="actBtn" @click="del(item)">
-               <img src="@/assets/imgs/icons/del.png" alt="">
-            </button>
+         <div v-for="item in state.getNewsObj" :key="item._id"
+            class="flex w-full justify-between rounded-[5px] p-5px bg-base-1 text-base-2">
+            <div class="flex items-center pl-20px text-20px">
+               {{ item.title }}
+            </div>
+            <div class="flex space-x-10px children:(cursor-pointer p-10px bg-base-3 rounded-[3px])">
+               <button @click="actNews = item">
+                  <img src="@/assets/imgs/icons/edit.png" alt="">
+               </button>
+               <button class="disabled:opacity-40" :disabled="actBtn" @click="del(item)">
+                  <img src="@/assets/imgs/icons/del.png" alt="">
+               </button>
+            </div>
          </div>
+         <transition name="blocks">
+            <pages-admin-form-news @close="actNews = ''" v-if="actNews" :act-news="actNews" />
+         </transition>
       </div>
-      <transition name="blocks">
-         <pages-admin-form-news @close="actNews = ''" v-if="actNews" :act-news="actNews" />
-      </transition>
    </div>
 </template>
 <script setup>
