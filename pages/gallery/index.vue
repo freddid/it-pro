8415b987cda792aa6b910a1393ab76f173447712
fred <ugodaycnzass1995@gmail.com>
2023-02-07 22:20:04 +0300

    fix

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -1,14 +1,14 @@
 <template>
-   <section class="lg:pt-72px pt-62px md:pb-100px pb-60px">
+   <section class="lg:pt-72px pt-62px md:pb-100px pb-60px overflow-hidden">
       <div class="container flex flex-col items-center md:pt-100px pt-40px">
          <UiSubtitle>Галлерея</UiSubtitle>
          <h2 class="h2T md:mb-120px mb-80px">Фото архив</h2>
          <div class="grid w-full text-center md:(grid-cols-4 p-0 gap-80px) pl-20px gap-50px">
             <div @click="$router.push(`/gallery/${i}`)" v-for="(image, i) in useDataStore().gallery" :key="i"
                class="text-2xl cursor-pointer">
-               <div class="relative mb-20px z-0">
+               <div class="relative mb-20px z-0 hover:z-10">
                   <kinesis-container class=" h-200px">
-                     <kinesis-element tag="img" class="w-full rounded-[20px] object-cover h-full " :src="image[0]"
+                     <kinesis-element tag="img" class="w-full rounded-[20px] object-cover h-full" :src="image[0]"
                         :strength="10" type="depth" />
                      <kinesis-element tag="img" :src="image[1]" :strength="-10" type="rotate" transformOrigin="50% 400%"
                         axis="x" class="-z-2 top-0 left-0 object-cover rounded-[20px] w-full h-full absolute" />
