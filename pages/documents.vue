42b556ad253f376f5d4ab815f829221bf10878b6
fred <ugodaycnzass1995@gmail.com>
2023-03-16 21:49:08 +0300

    fix

--- pages/documents.vue
+++ pages/documents.vue
@@ -5,11 +5,16 @@
             <UiSubtitle>Положения</UiSubtitle>
             <h2 class="h2T md:mb-120px mb-80px">Документы</h2>
          </div>
-         <div class="grid lg:(grid-cols-6 text-base) text-sm grid-cols-3 text-center gap-10px">
+         <div class="grid lg:(grid-cols-6 text-base) text-sm grid-cols-3 text-center gap-30px">
             <a href="/docs/Положение IT-Pro.docx"
-               class="flex flex-col transform items-center transition-all hover:(text-base-3 scale-110)">
+               class="flex flex-col transform items-center transition-all leading-normal hover:(text-base-3 scale-110)">
                <img class="mb-10px" src="@/assets/imgs/icon-docx.png" alt="">
                Положение IT-Pro
+            </a>
+            <a href="/docs/Типовые задания к олимпиаде.docx"
+               class="flex flex-col transform items-center transition-all leading-normal hover:(text-base-3 scale-110)">
+               <img class="mb-10px" src="@/assets/imgs/icon-docx.png" alt="">
+               Типовые задания к олимпиаде
             </a>
          </div>
       </div>
